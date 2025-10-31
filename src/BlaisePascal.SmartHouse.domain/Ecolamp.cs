9c3996223661e652f09241e10a88dd9c7425777e
dante.magalotti <dante.magalotti@PC4609>
2025-10-31 09:03:08 +0100

    commit EcoLamp classe

--- src/BlaisePascal.SmartHouse.domain/Ecolamp.cs
+++ src/BlaisePascal.SmartHouse.domain/Ecolamp.cs
@@ -7,12 +7,44 @@ namespace SmartHouse.domain
 {
     public class Ecolamp
     {
+        private int maxBrightness = 10;
+        private const int MIN_BRIGHTNESS = 1;
+
         public bool IsOn
         {
             get => default;
             set
             {
             }
+        }
+
+        public bool IsPowerSaveMode
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void switchOnOff()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void increaseBrightness()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void decreaseBrightness()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SwitchPowerSaveMode()
+        {
+            maxBrightness = 5; 
+            throw new System.NotImplementedException();
         }
     }
 }

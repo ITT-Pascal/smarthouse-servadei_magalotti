2a370b1a1414ba3057c5c48d84bead90425f64fb
dante.magalotti <dante.magalotti@PC4609>
2025-10-31 08:51:37 +0100

    commit EcoLamp

---
+++ src/BlaisePascal.SmartHouse.domain/Ecolamp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartHouse.domain
+{
+    public class Ecolamp
+    {
+        public bool IsOn
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}

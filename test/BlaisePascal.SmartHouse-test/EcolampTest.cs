b15f93c0049ef48740ff5c575bf2f0930b38d625
michele.servadei <michele.servadei.stud@ispascalcomandini.it>
2025-11-07 09:08:36 +0100

    Aggiornati Test Per Pulizia Codice

--- test/BlaisePascal.SmartHouse-test/EcolampTest.cs
+++ test/BlaisePascal.SmartHouse-test/EcolampTest.cs
@@ -13,68 +13,102 @@ namespace SmartHouse_test
             [Fact]
             public void switchIsOnOff_WhenIsOnIsFalse_ItChangesItToTrue()
             {
+                //Arrange
                 Ecolamp ecolamp = new Ecolamp();
+
+                //Act
                 ecolamp.switchOnOff();
+
+                //Assert
                 Assert.True(ecolamp.IsOn);
             }
 
             [Fact]
             public void switchIsOnOff_WhenIsOnIsTrue_ItChangesItToFalse()
             {
+                //Arrange
                 Ecolamp ecolamp = new Ecolamp();
+
+                //Act
                 ecolamp.switchOnOff();
                 ecolamp.switchOnOff();
+
+                //Assert
                 Assert.False(ecolamp.IsOn);
             }
 
             [Fact]
-            //&/7ecccococo
             public void increaseBrightness_WhenBrightnessIsLessThanMax_ItIncreasesByOne()
             {
+                //Assert
                 Ecolamp ecolamp = new Ecolamp();
+                //Act
                 ecolamp.switchOnOff();
                 ecolamp.decreaseBrightness();
                 ecolamp.increaseBrightness();
+
+                //Assert
                 Assert.Equal(10, ecolamp.Brightness);
             }
 
             [Fact]
             public void increaseBrightness_WhenBrightnessIsMax_ItRemainsMax()
             {
+                //Arrange
                 Ecolamp lamp = new Ecolamp();
+
+                //Assert
                 lamp.switchOnOff();
                 lamp.increaseBrightness();
+
+                //Act
                 Assert.Equal(10, lamp.Brightness);
             }
 
             [Fact]
             public void decreaseBrightness_WhenBrightnessIsMoreThanMin_ItDecreasesByOne()
             {
+                //Arrrange
                 Ecolamp lamp = new Ecolamp();
+
+                //Act
                 lamp.switchOnOff();
                 lamp.decreaseBrightness();
+
+                //Assert
                 Assert.Equal(9, lamp.Brightness);
             }
 
             [Fact]
             public void decreaseBrightness_WhenBrightnessIsMin_ItRemainsMin()
             {
+                //Arrange
                 Ecolamp lamp = new Ecolamp();
+
+                //Act
                 lamp.switchOnOff();
                 for (int i = 0; i < 11; i++)
                 {
                     lamp.decreaseBrightness();
                 }
+
+                 //Assert
                 Assert.Equal(1, lamp.Brightness);
             }
 
             [Fact]
             public void increaseBrightness_WhenPowerSaveModeIsActiveted_ItMaxBrightnessIs5()
             {
+
+                //Arrange
                 Ecolamp lamp = new Ecolamp();
+
+                //Act
                 lamp.switchOnOff();
                 lamp.SwitchPowerSaveMode();
+
                 
+                //Assert
                 Assert.Equal(5, lamp.maxBrightness);
             }
 
--- test/BlaisePascal.SmartHouse-test/LampTest.cs
+++ test/BlaisePascal.SmartHouse-test/LampTest.cs
@@ -7,17 +7,28 @@ namespace SmartHouse_test
         [Fact]
         public void switchIsOnOff_WhenIsOnIsFalse_ItChangesItToTrue()
         {
+
+            //Arrange
             Lamp lamp = new Lamp();
+
+            //Act
             lamp.switchOnOff();
+
+            //Assert
             Assert.True(lamp.IsOn);
         }
 
         [Fact]
         public void switchIsOnOff_WhenIsOnIsTrue_ItChangesItToFalse()
         {
+            //Arrange
             Lamp lamp = new Lamp();
+
+            //Act
             lamp.switchOnOff();
             lamp.switchOnOff();
+
+            //Assert
             Assert.False(lamp.IsOn);
         }
 
@@ -25,42 +36,62 @@ namespace SmartHouse_test
 
         public void increaseBrightness_WhenBrightnessIsLessThanMax_ItIncreasesByOne()
         {
-
+            //Arrange
             Lamp lamp = new Lamp();
+
+            //Act
             lamp.switchOnOff();
             lamp.decreaseBrightness();
             lamp.increaseBrightness();
+
+            //Assert
             Assert.Equal(10, lamp.Brightness);
         }
 
         [Fact]
         public void increaseBrightness_WhenBrightnessIsMax_ItRemainsMax()
         {
-
+            //Arrange
             Lamp lamp = new Lamp();
+
+            //Act
             lamp.switchOnOff();
             lamp.increaseBrightness();
+
+            //Assert
             Assert.Equal(10, lamp.Brightness);
         }
 
         [Fact]
         public void decreaseBrightness_WhenBrightnessIsMoreThanMin_ItDecreasesByOne()
         {
+
+            //Arrange
             Lamp lamp = new Lamp();
+
+            //Act
             lamp.switchOnOff();
             lamp.decreaseBrightness();
+
+            //Assert
             Assert.Equal(9, lamp.Brightness);
         }
 
         [Fact]
         public void decreaseBrightness_WhenBrightnessIsMin_ItRemainsMin()
         {
+
+            //Arrange
             Lamp lamp = new Lamp();
+
+            //Act
             lamp.switchOnOff();
             for (int i = 0; i < 11; i++)
             {
                 lamp.decreaseBrightness();
             }
+
+            //Assert
             Assert.Equal(1, lamp.Brightness);
         }
     }

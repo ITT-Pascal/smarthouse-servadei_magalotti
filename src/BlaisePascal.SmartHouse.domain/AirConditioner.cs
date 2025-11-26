2c1fc34758a669743f9a3e2c26107181935d6b32
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2025-11-26 10:59:55 +0100

    Added CctvStatus class, modified Cctv class, modified AirConditioner

--- src/BlaisePascal.SmartHouse.domain/AirConditioner.cs
+++ src/BlaisePascal.SmartHouse.domain/AirConditioner.cs
@@ -11,7 +11,8 @@ namespace SmartHouse.domain
         public bool IsOn { get; private set; }
         public string Name { get; private set; }
         public Guid IdAirConditioner { get; private set; }
-        public DeviceStatus Status { get; private set }
+        public DeviceStatus Status { get; private set; }
+        public double Temperature { get; private set; }
 
         public AirConditioner(string name)
         {
@@ -53,6 +54,43 @@ namespace SmartHouse.domain
             
         }
 
+        public void Toggle()
+        {
+            if (IsOn)
+            {
+                TurnOff();
+            }
+            else
+            {
+                TurnOn();
+            }
+        }
 
+        public void SetTemperature(double temperature)
+        {
+            if (!IsOn)
+            {
+                throw new InvalidOperationException("Cannot set temperature when the air conditioner is off.");
+            }
+            Temperature = temperature;
+        }
+
+        public void IncreaseTemperature(double increment)
+        {
+            if (!IsOn)
+            {
+                throw new InvalidOperationException("Cannot increase temperature when the air conditioner is off.");
+            }
+            Temperature += increment;
+        }
+
+        public void DecreaseTemperature(double decrement)
+        {
+            if (!IsOn)
+            {
+                throw new InvalidOperationException("Cannot decrease temperature when the air conditioner is off.");
+            }
+            Temperature -= decrement;
+        }
     }
 }
---
+++ src/BlaisePascal.SmartHouse.domain/CctvStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHouse.domain
+{
+    public enum CctvStatus
+    {
+        Normal = 0,
+        Termic = 1,
+        NightVision = 2,
+    }
+}

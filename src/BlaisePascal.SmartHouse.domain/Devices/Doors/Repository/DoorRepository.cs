0e56916d6b14bd7d1287bb8d2e61c233c749c3e4
michele.servadei <michele.servadei.stud@ispascalcomandini.it>
2026-02-06 09:47:58 +0100

    Added Repositories

---
+++ src/BlaisePascal.SmartHouse.domain/Devices/Doors/Repository/DoorRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlaisePascal.SmartHouse.Domain.Devices.LouminousDevices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Devices.Doors.DoorRepository
+{
+    public interface DoorRepository
+    {
+        void AddDoor(Door door);
+        void UpdateDoor(Door door);
+        void DeleteDoor(Guid id);
+        Door GetDoorById(Guid id);
+        List<Door> GetAllDoors();
+    }
+}
---
+++ src/BlaisePascal.SmartHouse.domain/Devices/LouminousDevices/Repositories/ILampRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Devices.LouminousDevices.Repositories
+{
+    public interface ILampRepository
+    {
+        void AddLamp(LampModel lamp);
+        void UpdateLamp(LampModel lamp);
+        void DeleteLamp(Guid id);
+        LampModel GetLampById(Guid id);
+        List<LampModel> GetAllLamps();
+    }
+}

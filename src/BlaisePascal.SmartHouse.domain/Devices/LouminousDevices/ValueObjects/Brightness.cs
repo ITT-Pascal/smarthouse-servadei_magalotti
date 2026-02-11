e8e576fccfbc75f2f93ee078f59c7bb86dffb560
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2026-02-11 10:59:26 +0100

    Added ValueObj folder

--- src/BlaisePascal.SmartHouse.domain/Devices/LouminousDevices/ValueObjects/Brightness.cs
+++ src/BlaisePascal.SmartHouse.domain/Devices/LouminousDevices/ValueObjects/Brightness.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlaisePascal.SmartHouse.Domain.Devices
+namespace BlaisePascal.SmartHouse.Domain.Devices.LouminousDevices.ValueObjects
 {
     public record Brightness
     {

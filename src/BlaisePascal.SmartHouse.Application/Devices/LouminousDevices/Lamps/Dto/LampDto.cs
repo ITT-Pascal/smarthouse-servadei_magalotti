f72cdfeb25aeffb247640badb7cbdbdada83256c
GabrieleTumaini <gabriele.tumaini.stud@ispascalcomandini.it>
2026-02-20 09:02:25 +0100

    Added LampDto

---
+++ src/BlaisePascal.SmartHouse.Application/Devices/LouminousDevices/Lamps/Dto/LampDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.LouminousDevices.Lamps.Dto
+{
+    public class LampDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DeviceStatus { get; set; }
+        public int Brightness { get; set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime LastModifiedAtUtc { get; set; }
+    }
+}

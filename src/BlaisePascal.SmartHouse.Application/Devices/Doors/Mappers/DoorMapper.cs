8221ee32e90b44b45180d932c69551256d847fe5
michele.servadei <michele.servadei.stud@ispascalcomandini.it>
2026-02-27 09:48:30 +0100

    re-Updated Door Applications

--- src/BlaisePascal.SmartHouse.Application/Devices/Doors/Mappers/DoorMapper.cs
+++ src/BlaisePascal.SmartHouse.Application/Devices/Doors/Mappers/DoorMapper.cs
@@ -1,4 +1,5 @@
 ﻿using BlaisePascal.SmartHouse.Application.Devices.Doors.Dto;
+using BlaisePascal.SmartHouse.Domain.Devices.Doors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,12 @@ namespace BlaisePascal.SmartHouse.Application.Devices.Doors.Mappers
 {
     public static class DoorMapper
     {
-        public static DoorDto ToDto(DoorDto door)
+        public static DoorDto ToDto(Door door)
         {
             return new DoorDto
             {
                 Id = door.Id,
                 Name = door.Name,
-                Password = door.Password,
                 IsLocked = door.IsLocked,
                 CreatedAtUtc = door.CreatedAtUtc,
                 LastModifiedAtUtc = door.LastModifiedAtUtc,
---
+++ src/BlaisePascal.SmartHouse.Application/Devices/Doors/Queries/GetAllDoorsQuery.cs
@@ -0,0 +1,34 @@
+﻿
+using BlaisePascal.SmartHouse.Application.Devices.Doors.Dto;
+using BlaisePascal.SmartHouse.Application.Devices.Doors.Mappers;
+using BlaisePascal.SmartHouse.Domain.Devices.Doors;
+using BlaisePascal.SmartHouse.Domain.Devices.Doors.DoorRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.Doors.Queries
+{
+    public class GetAllDoorsQuery
+    {
+        private readonly IDoorRepository _doorRepository;
+
+        public GetAllDoorsQuery(IDoorRepository doorRepository)
+        {
+            _doorRepository = doorRepository;
+        }
+
+        public List<DoorDto> Execute()
+        {
+            List<DoorDto> doors = new List<DoorDto>();
+            //return doors.Select(cctv => DoorMapper.ToDto(doors)).ToList();
+            foreach(Door door in _doorRepository.GetAll())
+            {
+                doors.Add(DoorMapper.ToDto(door));
+            }
+            return doors;
+        }
+    }
+}

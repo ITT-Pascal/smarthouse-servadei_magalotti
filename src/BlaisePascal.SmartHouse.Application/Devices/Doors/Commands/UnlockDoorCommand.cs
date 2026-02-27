5b3919ab18d6df05bffb2794f7782501580a226e
michele.servadei <michele.servadei.stud@ispascalcomandini.it>
2026-02-27 08:56:16 +0100

    update Door Application

---
+++ src/BlaisePascal.SmartHouse.Application/Devices/Doors/Commands/UnlockDoorCommand.cs
@@ -0,0 +1,32 @@
+﻿using BlaisePascal.SmartHouse.Domain.Devices.CCTVs.Repositories;
+using BlaisePascal.SmartHouse.Domain.Devices.Doors;
+using BlaisePascal.SmartHouse.Domain.Devices.Doors.DoorRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.Doors.Commands
+{
+    public class UnlockDoorCommand
+    {
+        private readonly IDoorRepository _doorRepository;
+
+        public UnlockDoorCommand(IDoorRepository doorRepository)
+        {
+            _doorRepository = doorRepository;
+        }
+
+        public void Execute(Guid doorId, string password, Door door)
+        {
+            var doorVar = _doorRepository.GetById(doorId);
+            if (door != null)
+            {
+                door.Unlock(password);
+                _doorRepository.Update(door);
+            }
+        }
+    }
+}
+
---
+++ src/BlaisePascal.SmartHouse.Application/Devices/Doors/Mappers/DoorMapper.cs
@@ -0,0 +1,28 @@
+﻿using BlaisePascal.SmartHouse.Application.Devices.Doors.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Application.Devices.Doors.Mappers
+{
+    public static class DoorMapper
+    {
+        public static DoorDto ToDto(DoorDto door)
+        {
+            return new DoorDto
+            {
+                Id = door.Id,
+                Name = door.Name,
+                Password = door.Password,
+                IsLocked = door.IsLocked,
+                CreatedAtUtc = door.CreatedAtUtc,
+                LastModifiedAtUtc = door.LastModifiedAtUtc,
+
+            };
+        }
+
+
+    }
+}
